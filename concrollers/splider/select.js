edd658a8c94315602f11c35f709e6f83655f9527
DESKTOP-OV6QNM5\xj <402555290@qq.com>
2019-01-11 02:21:21 +0800

    no message

--- concrollers/splider/select.js
+++ concrollers/splider/select.js
@@ -7,10 +7,10 @@ const select = async function (ctx, next) {
     indexPage.page = 1;
     var indexRes = await modules.spliderSelect.select(indexPage);
     var obj = {
-        title:'腾讯开奖号码',
-        description: '内容介绍',
-        author: 'author',
-        keywords: '关键词',
+        title:'腾讯开奖号码_QQ在线统计,腾讯在线统计_分分统计',
+        description: '提供腾讯在线人数的计算结果————即腾讯分分彩实时开奖结果，每分钟第5秒刷新数据。7*24小时不间断。',
+        author: '腾讯在线统计',
+        keywords: '腾讯在线统计,腾讯在线人数统计,qq在线统计,QQ在线统计,qq在线人数统计,QQ在线人数统计,分分统计',
         txOnlineNumber: indexRes.data[0].txOnlineNumber,
         lotteryNumber: indexRes.data[0].lotteryNumber,
         contentData: indexRes,
